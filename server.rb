432d8d8b6791a7698548be30e56a93860d23a54a
William Holt <wtholt27@gmail.com>
2015-01-15 16:19:42 -0500

    progressing with blog site

--- server.rb
+++ server.rb
@@ -1,7 +1,54 @@
 require 'sinatra'
+require 'data_mapper'
 
+DataMapper.setup(
+	:default,
+	'mysql://root@localhost/blogs'
+)
 
+class Blog
+	include DataMapper::Resource
+	property :id, Serial
+	property :title, String
+	property :date, String
+	property :blog, String
+end
+
+DataMapper.finalize.auto_upgrade!
 
 get '/' do
-	erb :index
+	@blogs = Blog.all
+	erb :index, layout: :layout
 end
+
+get '/newblog' do
+	erb :create_blog, layout: :layout
+end
+
+post '/create_blog' do
+	p params
+	@blog = Blog.new
+	@blog.title = params[:title]
+	@blog.date = params[:date]
+	@blog.blog = params[:blog]
+	@blog.save
+	redirect to '/'
+end
+
+get '/blog/:id' do
+	@blog = Blog.get params[:id]
+	erb :display_blogs
+end
+
+delete '/delete_blog/:id' do 
+	@blog = Blog.get params[:id]
+	@blog.destroy
+	redirect to '/'
+end
+
+
+
+
+
+
+
